7696ce90b2795660e0828fa6e2fb9d5b740eeb6a
Kchulka <kchulka008@seznam.cz>
2022-05-16 15:16:02 +0200

    updated, so you can now change window size or fullscreen

--- game.py
+++ game.py
@@ -4,6 +4,7 @@
 import pyglet
 from pyglet.window import key
 from pyglet import gl
+from pyglet import  window
 
 import yaml
 from yaml import Loader
@@ -27,16 +28,17 @@ gamesettings = gamesettings()
 run = False
 
 # -- window settings --
+display = pyglet.canvas.get_display()
+screens = display.get_screens()
 window_data = gamesettings.data.get("WINDOW_SETTINGS")
-window = pyglet.window.Window(width=window_data.get("WIDTH"), height=window_data.get("HEIGHT"), resizable=window_data.get("RESIZABLE"))
-
+if window_data.get("FULLSCREEN") == True:
+    window = pyglet.window.Window(width=window_data.get("WIDTH"), height=window_data.get("HEIGHT"), resizable=window_data.get("RESIZABLE"), fullscreen=window_data.get("FULLSCREEN"), screen=screens[int(window_data.get("MONITOR"))], style=pyglet.window.Window.WINDOW_STYLE_DIALOG )
+else:
+    window = pyglet.window.Window(width=window_data.get("WIDTH"), height=window_data.get("HEIGHT"), resizable=window_data.get("RESIZABLE") )
 # -- Resource variables --
 
 asteroid_images = "resources/meteors/['meteorBrown_big1.png', 'meteorBrown_med1.png', 'meteorBrown_small1.png', 'meteorBrown_tiny1.png']"
 asteroid_images += "resources/meteors/['meteorGrey_big2.png', 'meteorGrey_med2.png', 'meteorGrey_small2.png', 'meteorGrey_tiny2.png']"
-
-# -- Values --
-
 
 
 # -- Classes --
@@ -66,21 +68,12 @@ class Space:
 
 class SpaceObject:
 # Objects properities
-    def __init__(self, img='resources/spaceships/playerShip1_blue.png', x=0, y=0, speed=0, rotation=0):
-        self.image = pyglet.image.load(img)
-        self.image.anchor_x = self.image.width // 2
-        self.image.anchor_y = self.image.height // 2
-        self.sprite = pyglet.sprite.Sprite(self.image, batch=batch, group=foreground)
-        self.x = x
-        self.y = y
-        self.rotation = rotation
-        self.speed = speed
 
     def __init__(self, img, x=0, y=0, speed=0, rotation=0, group=None):
         self.image = pyglet.image.load(img)
         self.x = x
         self.y = y
-        self.centering_image()
+        #self.centering_image()
         if group == None:
             group=foreground
         self.sprite = pyglet.sprite.Sprite(img=self.image, batch=batch, group=group)
@@ -150,12 +143,37 @@ class SpaceShip(SpaceObject):
 
     def __init__(self, img, x=0, y=0, management=dict()):
         super().__init__(self.img, x, y)
+
+        self.sprite.image.width = window.height//self.ship.get("SIZE")
+        self.sprite.image.height = window.height//self.ship.get("SIZE")
+
+        self.centering_image()
+
         if len(management) == 0:
             self.management = {'left': self.controls_left, 'right': self.controls_right, 'forward': self.controls_forward, 'backward': self.controls_backward, "shoot": key.SPACE}
         else:
             self.management = management
 
+
+
+    def centering_image(self):
+        """ centering of image """
+        self.sprite.image.anchor_x = self.sprite.image.width//2
+        self.sprite.image.anchor_y = self.sprite.image.height//2
+        if self.x - self.image.anchor_x < 0:
+            self.x = self.image.anchor_x
+        elif self.x + self.image.anchor_x > window.width:
+            self.x = window.width - self.image.anchor_x
+        if self.y - self.image.anchor_y < 0:
+            self.y = self.image.anchor_y
+        elif self.y + self.image.anchor_y > window.height:
+            self.y = window.height - self.image.anchor_y
+
+
+
     def tik(self, dt):
+        self.sprite.image.width = window.height//self.ship.get("SIZE")
+        self.sprite.image.height = window.height//self.ship.get("SIZE")
         if self.management['left'] in pushed_keys:
             """ rotace vlevo """
             self.rotation -= dt * SpaceShip.ROTATION_SPEED
@@ -175,8 +193,6 @@ class SpaceShip(SpaceObject):
                 self.speed = 0
 
         super().tik(dt)
-
-
 
 # --- some keyboard things
 
@@ -231,7 +247,7 @@ foreground = pyglet.graphics.OrderedGroup(1)  # batch group
 def rungame():
     global Space
     Space = Space()
-    Space += SpaceShip(img=None, x=window.width // 2, y=window.height // 2)
+    Space += SpaceShip(img=SpaceShip.img, x=window.width // 2, y=window.height // 2)
     pyglet.clock.schedule_interval(Space.tik, 1/gamesettings.data.get("WINDOW_SETTINGS").get("TICKSPEED"))
 
 # -- run application --
