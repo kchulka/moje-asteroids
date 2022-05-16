77754a6cd85496872587608dd4a50e49502fab3f
Kchulka <kchulka008@seznam.cz>
2022-05-16 19:15:31 +0200

    v0.0.4

--- game.py
+++ game.py
@@ -10,7 +10,6 @@ import yaml
 from yaml import Loader
 
 import random
-import time
 import math
 
 # -- import settings from file
@@ -22,11 +21,6 @@ class gamesettings:
         #print(self.data)
 gamesettings = gamesettings()
 
-
-# -- game run
-
-run = False
-
 # -- window settings --
 display = pyglet.canvas.get_display()
 screens = display.get_screens()
@@ -35,11 +29,6 @@ if window_data.get("FULLSCREEN") == True:
     window = pyglet.window.Window(width=window_data.get("WIDTH"), height=window_data.get("HEIGHT"), resizable=window_data.get("RESIZABLE"), fullscreen=window_data.get("FULLSCREEN"), screen=screens[int(window_data.get("MONITOR"))], style=pyglet.window.Window.WINDOW_STYLE_DIALOG )
 else:
     window = pyglet.window.Window(width=window_data.get("WIDTH"), height=window_data.get("HEIGHT"), resizable=window_data.get("RESIZABLE") )
-# -- Resource variables --
-
-asteroid_images = "resources/meteors/['meteorBrown_big1.png', 'meteorBrown_med1.png', 'meteorBrown_small1.png', 'meteorBrown_tiny1.png']"
-asteroid_images += "resources/meteors/['meteorGrey_big2.png', 'meteorGrey_med2.png', 'meteorGrey_small2.png', 'meteorGrey_tiny2.png']"
-
 
 # -- Classes --
 
@@ -59,12 +48,9 @@ class Space:
         self.objects += [other]
         return self
 
-
     def tik(self, dt):
         for self.o in self.objects:
             self.o.tik(dt)
-
-
 
 class SpaceObject:
 # Objects properities
@@ -73,7 +59,6 @@ class SpaceObject:
         self.image = pyglet.image.load(img)
         self.x = x
         self.y = y
-        #self.centering_image()
         if group == None:
             group=foreground
         self.sprite = pyglet.sprite.Sprite(img=self.image, batch=batch, group=group)
@@ -81,10 +66,72 @@ class SpaceObject:
         self.speed = speed
         self.radius = max([self.sprite.width, self.sprite.height]) / 2
 
+
+    def out_of_window(self):
+        """ objekt mimo okno aplikace """
+        if self.x > window.width:
+            self.x = self.sprite.x = 0
+        elif self.x < 0:
+            self.x = self.sprite.x = window.width
+        elif self.y > window.height:
+            self.y = self.sprite.y = 0
+        elif self.y < 0:
+            self.y = self.sprite.y = window.height
+
+    def tik(self, dt):
+        self.out_of_window()
+        # -- Objects movement calculations --
+        self.x += self.speed * math.cos(self.rotation)
+        self.y += self.speed * math.sin(self.rotation)
+
+        # -- parametry sprite --
+        self.sprite.rotation = 90 - math.degrees(self.rotation)
+        self.sprite.x = self.x
+        self.sprite.y = self.y
+
+###############################################################################
+#                               SPACE SHIP
+###############################################################################
+
+class SpaceShip(SpaceObject):
+## -- space ship --
+    ship = gamesettings.data.get('ship')
+    # -- Movement values
+    ROTATION_SPEED  = ship.get('ROTATION_SPEED') # radians/sec
+    MAX_SPEED       = ship.get("MAX_SPEED")
+    ACCELERATION    = ship.get('ACCELERATION')
+    BREAKS          = ship.get('BREAKS')
+    # -- Movement values
+    controls = gamesettings.data.get('ship').get("controls")
+
+    controls_forward = int(controls.get("forward"))
+    controls_backward = int(controls.get("backward"))
+    controls_left = int(controls.get("left"))
+    controls_right = int(controls.get("right"))
+    controls_shoot = int(controls.get("shoot"))
+    # -- Space ship images
+    img             = 'resources/spaceships/playerShip1_blue.png'
+
+
+    def __init__(self, img, x=window.width//2, y=window.height//2, management=dict()):
+        super().__init__(self.img, x, y, rotation=math.pi / 2)
+        self.sprite.image.width = window.height//self.ship.get("SIZE")
+        self.sprite.image.height = window.height//self.ship.get("SIZE")
+        self.centering_image()
+        if len(management) == 0:
+            self.management = {'left': self.controls_left, 'right': self.controls_right, 'forward': self.controls_forward, 'backward': self.controls_backward, 'shoot': self.controls_shoot}
+        else:
+            self.management = management
+
+        self.lasers = []
+        self.shoot_delay = 0
+        super().__init__(img, x, y, rotation=math.pi/2)
+        #super().__init__(self.img, x, y)
+
     def centering_image(self):
         """ centering of image """
-        self.image.anchor_x = self.image.width // 2
-        self.image.anchor_y = self.image.height // 2
+        self.sprite.image.anchor_x = self.sprite.image.width//2
+        self.sprite.image.anchor_y = self.sprite.image.height//2
         if self.x - self.image.anchor_x < 0:
             self.x = self.image.anchor_x
         elif self.x + self.image.anchor_x > window.width:
@@ -105,94 +152,100 @@ class SpaceObject:
         elif self.y < 0:
             self.y = self.sprite.y = window.height
 
-# Space.HEIGHT bylo nahrazeno window.height
-
+    def __repr__(self):
+        return "SpaceShip({}, {}, {})".format(self.x, self.y, self.lasers)
 
     def tik(self, dt):
-        # -- Objects movement calculations --
-        self.x += self.speed * math.cos(self.rotation)
-        self.y += self.speed * math.sin(self.rotation)
-
-        # -- parametry sprite --
-        self.sprite.rotation = 90 - math.degrees(self.rotation)
-        self.sprite.x = self.x
-        self.sprite.y = self.y
-
         self.out_of_window()
-
-
-
-
-class SpaceShip(SpaceObject):
-## -- space ship --
-    ship = gamesettings.data.get('ship')
-    # -- Movement values
-    ROTATION_SPEED  = ship.get('ROTATION_SPEED') # radians/sec
-    ACCELERATION    = ship.get('ACCELERATION')
-    BREAKS          = ship.get('BREAKS')
-    # -- Movement values
-    controls = gamesettings.data.get('ship').get("controls")
-
-    controls_forward = int(controls.get("forward"))
-    controls_backward = int(controls.get("backward"))
-    controls_left = int(controls.get("left"))
-    controls_right = int(controls.get("right"))
-    # -- Space ship images
-    img             = 'resources/spaceships/playerShip1_blue.png'
-
-
-    def __init__(self, img, x=0, y=0, management=dict()):
-        super().__init__(self.img, x, y)
+        if self.shoot_delay > 0:
+            self.shoot_delay -= 1
 
         self.sprite.image.width = window.height//self.ship.get("SIZE")
         self.sprite.image.height = window.height//self.ship.get("SIZE")
-
         self.centering_image()
 
-        if len(management) == 0:
-            self.management = {'left': self.controls_left, 'right': self.controls_right, 'forward': self.controls_forward, 'backward': self.controls_backward, "shoot": key.SPACE}
-        else:
-            self.management = management
-
-
-
-    def centering_image(self):
-        """ centering of image """
-        self.sprite.image.anchor_x = self.sprite.image.width//2
-        self.sprite.image.anchor_y = self.sprite.image.height//2
-        if self.x - self.image.anchor_x < 0:
-            self.x = self.image.anchor_x
-        elif self.x + self.image.anchor_x > window.width:
-            self.x = window.width - self.image.anchor_x
-        if self.y - self.image.anchor_y < 0:
-            self.y = self.image.anchor_y
-        elif self.y + self.image.anchor_y > window.height:
-            self.y = window.height - self.image.anchor_y
-
-
-
-    def tik(self, dt):
-        self.sprite.image.width = window.height//self.ship.get("SIZE")
-        self.sprite.image.height = window.height//self.ship.get("SIZE")
         if self.management['left'] in pushed_keys:
             """ rotace vlevo """
-            self.rotation -= dt * SpaceShip.ROTATION_SPEED
-
+            self.rotation -= dt * self.ROTATION_SPEED
         if self.management['right'] in pushed_keys:
             """ rotace vpravo """
-            self.rotation += dt * SpaceShip.ROTATION_SPEED
+            self.rotation += dt * self.ROTATION_SPEED
 
         if self.management['forward'] in pushed_keys:
-            self.speed += dt * SpaceShip.ACCELERATION
-
-
+            if self.speed < self.MAX_SPEED - 1:
+                self.speed += dt * SpaceShip.ACCELERATION
+            else:
+                self.speed = self.MAX_SPEED
         if self.management['backward'] in pushed_keys:
             if self.speed > 1:
-                self.speed -= dt * SpaceShip.BREAKS
+                self.speed -= dt * self.BREAKS
             else:
                 self.speed = 0
 
+        if self.management['shoot'] in pushed_keys:
+            self.shoot()
+
         super().tik(dt)
+        ### wself.check_collision()
+
+        for laser in self.lasers:
+            laser.tik(dt)
+            #laser.check_collision()
+
+    def shoot(self):
+        """ výstřel """
+        if self.shoot_delay == 0:
+            self.shoot_delay = Laser.LASER_DELAY
+            self.lasers += [ Laser(self) ]
+
+    def delete(self):
+        [ laser.delete() for laser in self.lasers ]
+        super().delete()
+
+###############################################################################
+#                                  LASER
+###############################################################################
+
+
+class Laser(SpaceObject):
+    """ třída Laser - střela """
+
+    LASER = gamesettings.data.get("LASER")
+    LASER_IMAGE = 'resources/lasers/laserBlue01.png'
+    img = 'resources/lasers/laserBlue01.png'
+    LASER_SPEED = LASER.get("LASER_SPEED")
+    LASER_DELAY = LASER.get("LASER_DELAY")
+
+    def __init__(self, SpaceShip, img=None):
+        if img == None:
+            img = self.LASER_IMAGE
+        self.spaceship = SpaceShip
+        super().__init__(img, x=SpaceShip.x, y=SpaceShip.y, speed=SpaceShip.speed + Laser.LASER_SPEED,
+                         rotation=SpaceShip.rotation, group=lasers_group)
+
+    def __repr__(self):
+        return "Laser({}, {})".format(self.x, self.y)
+
+    def delete(self):
+        self.spaceship.lasers.remove(self)
+        del self
+
+    def out_of_window(self):
+        """ střela mimo okno """
+        if self.x < 0 or self.x > window.width or self.y < 0 or self.y > window.height:
+            self.delete()
+
+    def check_collision(self):
+        """ kolize laseru s ostatním vesmírným objektem """
+        for space_object in vesmir.objects:
+            if overlaps(self, space_object):
+                space_object.hit_by_laser(self)
+
+
+###############################################################################
+#                               Something else
+###############################################################################
+
 
 # --- some keyboard things
 
@@ -237,12 +290,15 @@ window.push_handlers(
 
 
 batch = pyglet.graphics.Batch()
-background = pyglet.graphics.OrderedGroup(0)  # batch group
-foreground = pyglet.graphics.OrderedGroup(1)  # batch group
+background   = pyglet.graphics.OrderedGroup(0)
+lasers_group = pyglet.graphics.OrderedGroup(1)
+foreground   = pyglet.graphics.OrderedGroup(2)
 
 
 
 # -- ticks --
+
+run = False
 
 def rungame():
     global Space
